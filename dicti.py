f0b6c0a25a73a5424b7af555da0fe3379b5d10f1
pruthvivikram <vikrampruthvi5@gmail.com>
2018-07-28 20:31:58 +0530

    01 - Create books working.
    What's next:
    1. If ISBN exists, increase the count of the books.
    2. If ISBN do not exist, create a book in its particular Key
    
    Hurdle:
    1.How and where to create counter?

---
+++ dicti.py
@@ -0,0 +1,13 @@
+
+
+#practicing dictionary
+
+d = {}
+
+d['i'] = ['I']
+print(d.get('i'))
+
+d.get('i').append('L')
+print(d.get('i'))
+
+d['l'] = []
--- shelf.py
+++ shelf.py
@@ -1,37 +1,38 @@
-
 # import statements
 from Book import book
 
+
 class shelf:
-
     # Required variables
-    shelves = {}        # Key : shelfname(String), Value: [list of book objects]
-
+    shelves = {}  # Key : shelfname(String), Value: [list of book objects]
 
     def create_book(self, b_isbn, b_name, b_lang, b_origin, b_authors, b_version, b_price):
-
-        # If the shelves is empty
-        if len(self.shelves.keys()) == 0 or str.upper(b_name) not in self.shelves.keys():
+        dict_key = str.upper(b_name[:2])
+        if len(self.shelves.keys()) == 0:                   # If the self.shelves is empty
             btemp = book(b_isbn, b_name, b_lang, b_origin, b_authors, b_version, b_price)
-            self.shelves[str.upper(b_name[:2])] = [btemp]
+            self.shelves[str.upper(dict_key)] = [btemp]
+            print(len(self.shelves.keys()))
         else:
-            # if not empty, find the key, get the
-            # For this iterate through the shelves, find the shelf, add it to the keys list
-            for i in self.shelves.keys():
-                # get the shelf values as list
-                temp_list = self.shelves.get(i)
-
+            btemp = book(b_isbn, b_name, b_lang, b_origin, b_authors, b_version, b_price)
+            print(dict_key not in self.shelves.keys())
+            if dict_key not in self.shelves.keys():
+                self.shelves[str.upper(dict_key)] = [btemp]
+            else:
+                self.shelves.get(dict_key).append(btemp)
 
     def get_books(self):
-        print("Printing all books\n" + "*"*18)
-        for key in self.shelves.keys():
-            book = self.shelves.get(key)
-            print(book[0].b_name)
-
+        print("\nPrinting all books\n" + "*" * 18)
+        shelves = self.shelves.keys()
+        for shelf in shelves:
+            print("shelf --> "+shelf)
+            for book in self.shelves.get(shelf):
+                print(book.b_name, book.b_isbn )
 
 
 shelf().create_book(11111111, "Linux Admin", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
-shelf().create_book(22222222, "Linux Admin", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
+shelf().create_book(22222222, "Windows Admin", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
+shelf().create_book(22222222, "Windows Admin", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
 shelf().create_book(33333333, "Python", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
-shelf().create_book(22222222, "Linux Admin", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
+shelf().create_book(22222222, "Pi Admin", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
+shelf().create_book(44444444, "Windows Admin", "English", "India", ["Rebello Williams"], "0.1a", 3050.00)
 shelf().get_books()
